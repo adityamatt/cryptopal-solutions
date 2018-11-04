121c11924608c7989b912ed7a517dbc7a682a1ce
Aditya <aditya.tiwarics@gmail.com>
2018-11-04 21:49:57 +0530

    Completed Challenge 10

--- Set2/10.cbc.py
+++ Set2/10.cbc.py
@@ -1,19 +1,40 @@
 import base64
+from Crypto.Cipher import AES
+from Crypto.Util.strxor import strxor
 
 #def decrypt_cbc(key,iv,input_str):
 
 key="YELLOW SUBMARINE"
+
+#iv
 iv="0000 0000 0000 0000"
 iv=iv.replace(" ","")
+
 fname="10.input"
 with open(fname) as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
-blocks=list()
+
 content = [x.strip() for x in content]
-block_string=""
+
+ciphertext=""
+
 for msg in content:
-    print len(msg),msg
-    block_string=block_string+base64.b64decode(msg)
-index=0
+    block_string=base64.b64decode(msg)
+    ciphertext=ciphertext+block_string
+    
+#block length
+k = 16
+
+#Decypting function
+machine = AES.new(key, AES.MODE_ECB)
+
+prev = iv
+
+n = len(ciphertext)
+for i in range(0,n,16):
+    cipherblock=ciphertext[i:i+16]
+    plainblock=machine.decrypt(cipherblock)
+    print strxor(plainblock,prev)
+    prev=cipherblock
 
