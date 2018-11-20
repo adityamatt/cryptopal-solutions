a52e4c3fa3c2bc3c08a50b68544ae62ed59d0205
Aditya <aditya.tiwarics@gmail.com>
2018-11-21 00:04:18 +0530

    Updated and Started new challenge

---
+++ Set2/16.cbc_bitflipping.py
@@ -0,0 +1,18 @@
+def valid_padding(str1):
+    n = len(str1)
+    if ord(str1[-1])>15:
+        return str1
+    padding = ord(str1[-1])
+    print n,padding
+    if padding>n:
+        raise ValueError('Invalid padding')
+    for i in range(1,padding+1):
+#        print ord(str1[-1]),padding
+        if ord(str1[-i])!=padding:
+            raise ValueError('Invalid padding')
+    return str1[0:n-padding]
+
+
+a= "ICE ICE BABY\x01\x02\x03\x04"
+print a
+print valid_padding(a)
