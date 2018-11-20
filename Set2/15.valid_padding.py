bf2bdef7d5c4c99e9c0d78b64deb82940225b816
Aditya <aditya.tiwarics@gmail.com>
2018-11-21 00:01:57 +0530

    Updated Challenge 15

--- Set2/15.valid_padding.py
+++ Set2/15.valid_padding.py
@@ -1,17 +1,10 @@
 def valid_padding(str1):
     n = len(str1)
-    if ord(str1[-1])>15:
-        return str1
-    padding = ord(str1[-1])
-    print n,padding
-    if padding>n:
-        raise ValueError('Invalid padding')
-    for i in range(1,padding+1):
-#        print ord(str1[-1]),padding
-        if ord(str1[-i])!=padding:
-            raise ValueError('Invalid padding')
-    return str1[0:n-padding]
-
-a= "ICE ICE BABY\x04\x04\x04\x04"
+    last_char = ord(str1[-1])
+    if last_char==0 or str1[-last_char:]!=chr(last_char)*last_char:
+        raise ValueError("Bad padding")
+    return str1[0:n-last_char]
+    
+a= "ICE ICE BAB\x04\x04\x04\x04"
 print a
 print valid_padding(a)
